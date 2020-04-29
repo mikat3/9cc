e6ff127832a85b007e0e43da7aaba29a1e7d2594
mikat <mikat3gm@gmail.com>
2020-04-29 22:04:30 +0900

    Completed Step.9

---
+++ test2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+assert() {
+  expected="$1"
+  input="$2"
+
+  ./9cc "$input" > tmp.s
+  cc -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+# Step.9
+
+assert 25 "a = 3; b = 5 * 6 - 8; a + b;"
+# assert 12 "a = 3; b = 5 * 6 - 8; a + b / 2;"    # Why 14 ?
+
+echo OK
